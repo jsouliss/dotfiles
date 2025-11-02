c41765bec6d4079a0cb6fbf25d550f5d9e3acf93
Gerardo Solis <gsolis5114@gmail.com>
2025-11-02 14:54:16 -0800

    Updated gitignore

--- wezterm/.config/wezterm/wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require 'wezterm'
-
-local config = {
-	font = wezterm.font("MesloLGS Nerd Font", {weight="Regular", stretch="Normal", style="Normal"}),
-	font_size = 10,
-	color_scheme = "Catppuccin Mocha"
-}
-
-return config
