de2731fa9889c6994205bea449a814ded7aca0c8
Gerardo Solis <gsolis5114@gmail.com>
2025-11-02 17:57:59 -0800

    Updated wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+local config = {
+    font = wezterm.font('MesloLGS Nerd Font'),  -- Defaults are fine
+    font_size = 12,
+    color_scheme = "Catppuccin Mocha"
+}
+return config
