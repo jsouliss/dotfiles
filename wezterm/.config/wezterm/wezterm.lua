b7934efd275e65d01d480ce62e9b1031064368d0
Gerardo Solis <gsolis5114@gmail.com>
2025-11-07 21:02:18 -0800

    Updated wezterm config
    Updated wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,18 @@
-local wezterm = require 'wezterm'
-local config = {
-    font = wezterm.font('MesloLGS Nerd Font'),  -- Defaults are fine
-    font_size = 11,
-    color_scheme = "Catppuccin Mocha"
-}
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+config.font = wezterm.font("MesloLGS Nerd Font") -- Defaults are fine
+config.font_size = 11
+config.color_scheme = "Catppuccin Mocha"
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+-- Finally, return the configuration to wezterm:
 return config
