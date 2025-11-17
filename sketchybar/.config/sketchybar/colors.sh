86edb38a646c71ea58e45996829c9e366b528ce5
Jerry Solis <jsouliss@users.noreply.github.com>
2025-11-16 17:15:24 -0800

    Updated sketchybar

---
+++ sketchybar/.config/sketchybar/colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export WHITE=0xffffffff         # White
+
+# -- Scheme --
+export BAR_COLOR=0xff1e1e2e     # Mocha Base Color (#1e1e2e)
+# export BAR_COLOR=0xffb4befe   # Lavender (#b4befe)
+export ITEM_BG_COLOR=0xfff38ba8 # Pink/Rose (#f38ba8)
+export ACCENT_COLOR=0xfff9e2af  # Cream/Yellow (#f9e2af)
