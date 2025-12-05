1f3b276616190008818c8733857922e44062055b
Gerardo Solis <gsolis5114@gmail.com>
2025-12-05 07:11:09 -0800

    Added p10k

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,21 +5,20 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
-config.font = wezterm.font("MesloLGS Nerd Font") -- Defaults are fine
-config.font_size = 11
+-- config.font = wezterm.font("MesloLGS Nerd Font") -- Defaults are fine
+config.font_size = 9
 config.color_scheme = "Catppuccin Mocha"
 
 -- Window configuration.
-config.hide_tab_bar_if_only_one_tab = true
+-- config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+-- config.macos_window_background_blur = 10
 
 -- Window padding configuration.
 config.window_padding = {
 	left = 0,
 	right = 0,
-	top = 55,
+	top = 0,
 	bottom = 0,
 }
 
