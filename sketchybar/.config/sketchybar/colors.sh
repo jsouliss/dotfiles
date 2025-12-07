621d9d5e6e96dd1a1c51bab84992431614324c2c
Gerardo Solis <gsolis5114@gmail.com>
2025-12-07 08:47:30 -0800

    Updated kitty config

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -2,7 +2,13 @@
 
 export WHITE=0xffffffff          # White
 
-# -- Scheme --
-export BAR_COLOR=0xff33375a      # Grey
-export ITEM_BG_COLOR=0xffff5555  # Red
-export ACCENT_COLOR=0xffff5555   # Red
+# -- Scheme 1 --
+# export BAR_COLOR=0xff282a36      # Dark Grey
+# export ITEM_BG_COLOR=0xffbd93f9  # Purple
+# export ACCENT_COLOR=0xffbd93f9   # Purple
+
+
+# -- Scheme 2 --
+export BAR_COLOR=0xff1e1e2e
+export ITEM_BG_COLOR=0xff7287fd
+export ACCENT_COLOR=0xff7287fd
